1bb7c062fb8b059fededa34a8bc9230d9a300b10
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-09-24 13:41:00 +0200

    Started adding tests (WIP)

---
+++ VERA/VERAApp/VERATests/NavigationCoordinatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Vonage on 24/9/25.
+//
+
+import Foundation
+import Testing
+@testable import VERA
+
+@Suite("NavigationCoordinator tests")
+struct NavigationCoordinatorTests {
+    
+    
+    // MARK: SUT
+    
+    func makeSUT() -> NavigationCoordinator {
+        NavigationCoordinator()
+    }
+}
