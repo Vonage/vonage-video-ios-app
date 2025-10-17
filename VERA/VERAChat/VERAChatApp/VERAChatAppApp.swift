9964b8649eea674b7321bd8f55262c50cb37d86e
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-10-17 12:27:52 +0200

    Adds a message in the VERAChatApp when a message is sent

--- VERA/VERAChat/VERAChatApp/VERAChatAppApp.swift
+++ VERA/VERAChat/VERAChatApp/VERAChatAppApp.swift
@@ -14,6 +14,13 @@ struct VERAChatAppApp: App {
     init() {
         let repository = DefaultChatMessagesRepository(
             messages: ChatMessage.sampleMessages)
+
+        repository.onSendMessage = { [weak repository] message in
+            repository?.addMessage(
+                .init(username: "Me", message: message, date: Date())
+            )
+        }
+
         chatFactory = ChatFactory(
             chatMessagesRepository: repository)
     }
