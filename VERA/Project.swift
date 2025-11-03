7124f5c87c9d82335f001a6a2b03174ad403d1cd
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-11-03 22:08:18 +0100

    Injecting `CFBundleShortVersionString` and `CFBundleVersion`

--- VERA/Project.swift
+++ VERA/Project.swift
@@ -46,8 +46,6 @@ private func createDependencies() -> [TargetDependency] {
 private func createBuildSettings() -> Settings {
     var baseSettings: [String: SettingValue] = [:]
 
-    baseSettings["MARKETING_VERSION"] = "1.0"
-
     if isChatEnabled() {
         baseSettings["CHAT_ENABLED"] = "1"
         baseSettings["SWIFT_ACTIVE_COMPILATION_CONDITIONS"] = "$(inherited) CHAT_ENABLED"
@@ -95,6 +93,8 @@ let project = Project(
                         "VERA needs access to your camera to share your video during video calls and meetings.",
                     "NSMicrophoneUsageDescription":
                         "VERA needs access to your microphone to share your audio during video calls and meetings.",
+                    "CFBundleShortVersionString": "$(MARKETING_VERSION)",
+                    "CFBundleVersion": "$(CURRENT_PROJECT_VERSION)",
                 ].merging(combinedPlistValues(), uniquingKeysWith: { _, new in new })),
             sources: ["VERAApp/VERA/App/**"],
             resources: ["VERAApp/VERA/Resources/**"],
--- VERA/Scripts/regenerateSigningConfig.sh
+++ VERA/Scripts/regenerateSigningConfig.sh
@@ -1,2 +1,4 @@
 mkdir -p Config
 echo "DEVELOPMENT_TEAM = $DEVELOPMENT_TEAM" > Config/Signing.xcconfig
+echo "MARKETING_VERSION = $MARKETING_VERSION" >> Config/Signing.xcconfig
+echo "CURRENT_PROJECT_VERSION = $CURRENT_PROJECT_VERSION" >> Config/Signing.xcconfig
