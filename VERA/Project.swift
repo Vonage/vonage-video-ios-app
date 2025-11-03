876e152b44534ff12971491f1da7eb1f62229764
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-11-03 14:52:08 +0100

    Set marketing version on VERA project

--- VERA/Project.swift
+++ VERA/Project.swift
@@ -46,6 +46,8 @@ private func createDependencies() -> [TargetDependency] {
 private func createBuildSettings() -> Settings {
     var baseSettings: [String: SettingValue] = [:]
 
+    baseSettings["MARKETING_VERSION"] = "1.0"
+    
     if isChatEnabled() {
         baseSettings["CHAT_ENABLED"] = "1"
         baseSettings["SWIFT_ACTIVE_COMPILATION_CONDITIONS"] = "$(inherited) CHAT_ENABLED"
