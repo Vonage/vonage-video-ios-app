a576267dfd75788d88dc23de7ecb94ffa9210eac
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-03-31 14:43:40 +0200

    Call update participant after the subscribe change

--- VERA/VERAVonage/VERAVonage/VonageSubscriber.swift
+++ VERA/VERAVonage/VERAVonage/VonageSubscriber.swift
@@ -211,6 +211,8 @@ public class VonageSubscriber: NSObject {
         guard subscriberDidConnect else { return }
 
         otSubscriber.subscribeToVideo = visible
+        
+        updateParticipant()
     }
 
     /// Enables or disables audio subscription.
