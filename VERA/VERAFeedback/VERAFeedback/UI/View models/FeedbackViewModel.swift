217a9a5da0481a43937e476f08eae2f55589e36c
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-06-03 14:01:09 +0200

    feat: Add VERAFeedback feature module
    - Module: VERAFeedback (VERA/VERAFeedback/)
    - Config: meetingRoomSettings.allowFeedback = true
    - Flag: FEEDBACK_ENABLED
    - Updated: app-config.json, VERA/Project.swift, ci.yml

---
+++ VERA/VERAFeedback/VERAFeedback/UI/View models/FeedbackViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Vonage on 3/6/26.
+//
+
+import Foundation
+
+/// View model for FeedbackView.
+@MainActor
+public final class FeedbackViewModel: ObservableObject {
+    // TODO: Add published properties and dependencies
+
+    public init() {}
+}
