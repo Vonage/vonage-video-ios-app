f3ee387f965f2083dd93d155b6338ab3fe643729
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-06-05 16:48:08 +0200

    VIDSOL-865: Feedback foundation (#164)
    Co-authored-by: aos-svg <artur.osinski@vonage.com>

---
+++ VERA/VERAFeedback/VERAFeedback/UI/View models/FeedbackViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Vonage on 3/6/26.
+//
+
+import Foundation
+
+/// View model for FeedbackView.
+@MainActor
+public final class FeedbackViewModel: ObservableObject {
+    // TODO: Add published properties and dependencies
+
+    public init() {
+        // TODO: Add initialisation, inject usecase
+    }
+}
---
+++ VERA/VERAFeedback/VERAFeedback/Wireframe/FeedbackFactory.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Vonage on 3/6/26.
+//
+
+import Foundation
+
+/// Factory for creating the Feedback feature components.
+public enum FeedbackFactory {
+    // TODO: Add factory methods
+}
