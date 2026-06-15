22d194f132281b4ae9bd68ce0883dff085a59e6e
aos-svg <artur.osinski@vonage.com>
2026-06-15 16:37:15 +0200

    VIDSOL-867 add constants

--- VERA/VERAFeedback/VERAFeedback/UI/View models/FeedbackFormViewModel.swift
+++ VERA/VERAFeedback/VERAFeedback/UI/View models/FeedbackFormViewModel.swift
@@ -6,35 +6,57 @@ import Combine
 
 class FeedbackFormViewModel: ObservableObject {
 
-    enum Const {
+    enum Constants {
         static let maxStandardFieldChars = 100
         static let maxDescriptionChars = 1000
+        
+        static let titleKey = String(localized: "Title")
+        static let titleFieldText = String(localized: "When you noticed this issue, what where you trying to do?")
+        static let nameKey = String(localized: "Name")
+        static let nameFieldText = String(localized: "Tell us your name")
+        static let descriptionKey = String(localized: "Description")
+        static let descriptionFieldText = String(localized: "Describe your issue")
+        static let infoKey = String(localized: "Info")
+        static let infoFieldText = String(localized: "Please do not include any sensitive information.")
+        static let imageKey = String(localized: "Image")
+        static let imageFieldText = String(localized: "A screenshot will help us better understand the issue. (optional)")
+        
+        static let formTitle =  String(localized: "Report issue")
     }
 
-    let title = String(localized: "Report issue")
+    let title = Constants.formTitle
     @Published var showValidationErrors = false
     @Published var feedbackFields = [
         FeedbackFieldViewModel(
-            maxChars: Const.maxStandardFieldChars,
-            title: String(localized: "When you noticed this issue, what where you trying to do?"),
-            key: String(localized: "Title"), type: .text
+            maxChars: Constants.maxStandardFieldChars,
+            title: Constants.titleFieldText,
+            key: Constants.titleKey,
+            type: .text
         ),
         FeedbackFieldViewModel(
-            maxChars: Const.maxStandardFieldChars,
-            title: String(localized: "Tell us your name"), key: String(localized: "Name"), type: .text
+            maxChars: Constants.maxStandardFieldChars,
+            title: Constants.nameFieldText,
+            key: Constants.nameKey,
+            type: .text
         ),
         FeedbackFieldViewModel(
-            maxChars: Const.maxDescriptionChars,
-            title: String(localized: "Describe your issue"), key: String(localized: "Description"), type: .text
+            maxChars: Constants.maxDescriptionChars,
+            title: Constants.descriptionFieldText,
+            key: Constants.descriptionKey,
+            type: .text
         ),
         FeedbackFieldViewModel(
-            title: "", key: String(localized: "Info"), type: .info,
-            value: String(localized: "Please do not include any sensitive information."),
+            title: "",
+            key: Constants.infoKey,
+            type: .info,
+            value: Constants.infoFieldText,
             isRequired: false
         ),
         FeedbackFieldViewModel(
-            title: "", key: String(localized: "Image"), type: .image,
-            value: String(localized: "A screenshot will help us better understand the issue. (optional)"),
+            title: "",
+            key: Constants.imageKey,
+            type: .image,
+            value: Constants.imageFieldText,
             isRequired: false
         ),
     ]
