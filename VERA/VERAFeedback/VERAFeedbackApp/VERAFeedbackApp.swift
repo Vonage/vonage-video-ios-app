b95bac6c75d92a2f6e8c1bd49abb1ea23823b977
jorgesanmartin-vng <jorge.sanmartin@vonage.com>
2026-06-30 09:50:36 +0200

    chore: Correct format files

--- VERA/VERAFeedback/VERAFeedbackApp/VERAFeedbackApp.swift
+++ VERA/VERAFeedback/VERAFeedbackApp/VERAFeedbackApp.swift
@@ -11,7 +11,7 @@ struct VERAFeedbackDemoApp: App {
         WindowGroup {
             NavigationStack {
                 FeedbackSheetContent(
-                  feedbackReportUseCase: NullFeedbackReportUseCase()
+                    feedbackReportUseCase: NullFeedbackReportUseCase()
                 )
             }
         }
