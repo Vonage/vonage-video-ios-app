4816518ab286965ef613bab0be33232c87889680
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-07-29 13:47:09 +0200

    Updated snapshot tests

--- VERA/VERASettings/VERASettings/UI/View/SettingsToolbarButton.swift
+++ VERA/VERASettings/VERASettings/UI/View/SettingsToolbarButton.swift
@@ -30,6 +30,8 @@ public typealias OnLaunchView = () -> SettingsView
 /// ```
 public struct SettingsToolbarButton: View {
 
+    @Environment(\.meetingRoomTheme) private var theme
+
     /// Closure for creating the settings view when the button is tapped.
     /// If `nil`, the button will show the sheet but with no content.
     private let makeSettingsView: OnLaunchView?
@@ -55,6 +57,7 @@ public struct SettingsToolbarButton: View {
         .sheet(isPresented: $showSettings) {
             makeSettingsView?()
                 .presentationDetents([.large])
+                .opaquePresentationBackground(theme.background)
         }
     }
 }
