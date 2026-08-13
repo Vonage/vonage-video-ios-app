6f0759e406d87c2dc37119c51be95b3b1ed36c22
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-08-13 16:31:07 +0200

    Removed unused factory method

--- VERA/VERAOKTA/VERAOKTA/Wireframe/OKTAFactory.swift
+++ VERA/VERAOKTA/VERAOKTA/Wireframe/OKTAFactory.swift
@@ -16,14 +16,6 @@ public final class OKTAFactory {
     }
 
     @MainActor
-    public func makeSignInView(
-        providers: [IDProvider],
-        onProviderSelected: @escaping (IDProvider) async throws -> Void
-    ) -> some View {
-        SignInView(providers: providers, onProviderSelected: onProviderSelected)
-    }
-
-    @MainActor
     public func makeNavBarAuthButtonViewModel(
         onLoginTapped: @escaping () -> Void,
         onLogoutTapped: @escaping () async -> Void
