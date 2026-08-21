1e662c8df2d6deb9aaae351524927bc79ab8344f
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-08-21 14:03:38 +0200

    Version bump

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Vonage/vonage-video-client-sdk-swift",
-            .upToNextMinor(from: "2.34.1")
+            .upToNextMinor(from: "2.35.1")
         ),
         .package(
             url: "https://github.com/Vonage/vonage-client-sdk-video-transformers",
